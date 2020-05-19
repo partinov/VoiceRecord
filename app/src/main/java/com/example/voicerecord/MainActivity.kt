2d5cb199afb6a8fc6ed33713e715ea75195dc412
partinov <partinov@gmail.com>
2020-05-19 17:41:05 +0300

    Added recording files playback and seek bar functionality

--- app/src/main/java/com/example/voicerecord/MainActivity.kt
+++ app/src/main/java/com/example/voicerecord/MainActivity.kt
@@ -1,66 +1,110 @@
 package com.example.voicerecord
 
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
-import android.widget.Adapter
+import android.os.Handler
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
+    private var  mediaPlayer: MediaPlayer? = null
     private var mediaRecorder: MediaRecorder? = null
     private var recording: Boolean = false
     private var adapter: ArrayAdapter<String>? = null
+    private var currentSong: String? = null
+    private var handler: Handler = Handler()
+    private lateinit var runnable:Runnable
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Check for permissions.
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // If permissions aren't granted, request them.
+            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions,0)
+        } // if
+
+        list.setOnItemClickListener { parent, view, position, id ->
+            currentSong = (getExternalFilesDir(null)?.absolutePath) + "/" + adapter?.getItem(position)
+            song_name.text = adapter?.getItem(position)
+        }
+
         record_button.setOnClickListener {
-            // Check for permissions.
-            if (ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
-                    && ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // If permissions aren't granted, request them.
-                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                ActivityCompat.requestPermissions(this, permissions,0)
-            } // if
-            else {
-                startRecording()
-            } // else
+           startRecording()
         } // record_button.setOnClickListener
 
         stop_button.setOnClickListener {
             stopRecording()
         } // stop_button.setOnClickListener
 
-        list.setOnItemClickListener { parent, view, position, id ->
-            val element = adapter?.getItem(position)
+        play_button.setOnClickListener {
+            playSong()
+        }
 
-
+        stop_playing_button.setOnClickListener {
+            stopSong()
         }
 
         // Display the files from the directory in the GUI.
         updateList()
     } // onCreate
 
+    private fun playSong(){
+        val fis = FileInputStream(File(currentSong))
+
+        mediaPlayer = MediaPlayer()
+        mediaPlayer?.setDataSource(fis.fd)
+        mediaPlayer?.prepare()
+        mediaPlayer?.start()
+
+        initialiseSeekBar()
+    }
+
+    private fun initialiseSeekBar(){
+        seekBar.max = mediaPlayer?.duration!!
+        seekBar.progress = mediaPlayer?.currentPosition!!
+
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer?.currentPosition!!
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    private fun stopSong(){
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+
+        handler.removeCallbacks(runnable)
+    }
+
     // A function that can be called to update the list.
     private fun updateList(){
         // Get the filenames of the desired directory as a list.
-        val filesList = File((getExternalFilesDir(null)?.absolutePath ?: null)).list()
+        val filesList = File((getExternalFilesDir(null)?.absolutePath)).list()
         // Put the list into the adapter so it could be listed in the GUI.
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, filesList)
         // Add adapter to the list.
@@ -70,11 +114,11 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     private fun startRecording() {
         //Get current time and date in proper format and output dir.
-        val time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss_dd-MM-yyyy"))
-        val outputDir = getExternalFilesDir(null)?.absolutePath ?: null
+        val time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH:mm:ss"))
+        val outputDir = getExternalFilesDir(null)?.absolutePath
 
         // Set output path.
-        val output = "$outputDir/Recording_$time.mp3"
+        val output = "$outputDir/Recording-$time.mp3"
 
         // Set up the media recorder.
         mediaRecorder = MediaRecorder().apply {
