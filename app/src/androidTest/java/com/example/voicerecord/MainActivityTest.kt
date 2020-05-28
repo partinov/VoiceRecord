ce829cc889f532603514339487dcfec278f52e6f
partinov <partinov@gmail.com>
2020-05-28 13:43:39 +0300

    Set up project for Junit5 tests

---
+++ app/src/androidTest/java/com/example/voicerecord/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.voicerecord
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainActivityTest {
+
+    @org.junit.jupiter.api.Test
+    fun onCreate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onRequestPermissionsResult() {
+    }
+}
