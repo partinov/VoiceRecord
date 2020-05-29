0dc66c62faa8e66dd438da48a971a0f19cd26518
partinov <partinov@gmail.com>
2020-05-29 16:19:55 +0300

    More setting up project for JUnit 5 tests.

--- app/src/test/java/com/example/MainActivityTest.kt
+++ app/src/test/java/com/example/MainActivityTest.kt
@@ -1,14 +1,17 @@
-package com.example.voicerecord
+package com.example
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
 internal class MainActivityTest {
 
     @org.junit.jupiter.api.Test
     fun onCreate() {
+        assertTrue(true)
     }
 
     @org.junit.jupiter.api.Test
     fun onRequestPermissionsResult() {
+        assertTrue(false)
     }
 }
